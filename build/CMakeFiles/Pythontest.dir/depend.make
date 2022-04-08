1d0ad5c0bf8a1af1707a931df8e6df0495e49f0d
hongfeng <1256568522@qq.com>
2022-04-08 16:08:59 +0800

    delete build

--- build/CMakeFiles/Pythontest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Pythontest.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/hongfeng/Storage/Code/test
-# Build directory: /media/hongfeng/Storage/Code/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
